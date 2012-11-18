9ecd60e7e467953cab5e2f70896cdd584ced361a
Artem KAZNATCHEEV <akazna@lab6-10.CS.McGill.CA>
2012-11-18 11:10:47 -0500

    Finished the deathBirth and birthDeath

--- birthDeath.m
+++ birthDeath.m
@@ -1,10 +1,25 @@
-function [genotypes ages base_fitnesses fitnesses] = birthDeath( adjmx, ...
-    genotypes, w, fitnesses, pmod, num_strat, mutFun )
+function [genotypesNew ages base_fitnesses fitnesses] = birthDeath( adjmx, ...
+    genotypesOld, w, fitnesses, pmod, reproduce)
 %BIRTHDEATH Summary of this function goes here
-%   Detailed explanation goes here
+%   reproduce is a function
 
+num_list = 1:length(genotypesOld);
 
+genotypesNew = genotypesOld;
 
+agents_cdf = cumsum(fun2pdf(fitness,w));
 
+for i=1:floor(length(genotypesOld)*pmod),
+    %select the first agent that passes the cdf threshold
+    choices = neighbours_indices(agents_cdf > rand);
+    parent = choices(1);
+    
+    %get the list of parent's neighbours
+    neighbours_indices = num_list(adjmx(parent,:)==1);
+    
+    %pick a neighbour uniformly at random.
+    child = neighbours_indices(ceil(rand*length(neighbours_indices)));
+    
+    genotypesNew(child) = reproduce(genotypesOld(parent));
 end
 
