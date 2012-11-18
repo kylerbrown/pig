f5cc4dfd8503ceea119bfe23c9ba9cd22e1ff632
Kyler BROWN <kbrown53@lab6-11.CS.McGill.CA>
2012-11-18 11:08:26 -0500

    completed indMutReproduce

--- reproduce.m
+++ reproduce.m
@@ -1,8 +1,9 @@
-function [ new_genotypes ] = indMutReproduce( genotypes, pmut )
+function [ genotypes ] = indMutReproduce( genotypes, pmut )
 %indMutReproduce 
 %   Detailed explanation goes here
 % genotypes, rows are agents, collumns are genes
 % first gene is strat, second is mat_age
-new_genotypes=genotypes;
-mut=rand(size(genotypes)<
+genotypes=genotypes;
+mut=rand(size(genotypes)) < repmat(pmut,length(genotypes),1);
+genotypes(:,1)
 
