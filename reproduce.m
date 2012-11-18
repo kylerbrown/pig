460710790362047a184f50846be1657aeacf7e80
Kyler Brown <kyler@bravo>
2012-11-18 13:47:30 -0600

    removed reproduce

--- reproduce.m
+++
@@ -1,9 +0,0 @@
-function [ genotypes ] = indMutReproduce( genotypes, pmut )
-%indMutReproduce 
-%   Detailed explanation goes here
-% genotypes, rows are agents, collumns are genes
-% first gene is strat, second is mat_age
-genotypes=genotypes;
-mut=rand(size(genotypes)) < repmat(pmut,length(genotypes),1);
-genotypes(:,1)
-
