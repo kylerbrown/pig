08261e9543813989fa9d72763ba04dc1c37fb140
Kyler Brown <kyler@bravo>
2012-11-19 16:14:33 -0500

    testing batch script

--- batch_run.m
+++ batch_run.m
@@ -1,12 +1,13 @@
 % this script runs and saves data
 
 num_iterations = 1;
-
+datadir = 'home/2007/kbrown53/pigdata/';
+dataname = 'test';
 for i = 1:num_iterations
   latticeRun
-  data_files = ls('../pigdata');
-  save_num = size(data_files,1)+1;
-  save(['../pigdata/data_',sprintf('%04d',save_num)])
+  data_files = dir(datadir);
+  save_num = size(data_files,1)-1;
+  save([datadir,dataname,sprintf('%04d',save_num)]);
 end
 
 exit
