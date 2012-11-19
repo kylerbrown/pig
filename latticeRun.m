1c8c7ec82895cdf48d73ecff8c8578906bdeb319
Kyler Brown <kyler@bravo>
2012-11-19 18:16:18 -0500

    foo

--- latticeRun.m
+++ latticeRun.m
@@ -2,34 +2,44 @@
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-n = 10*10;
-max_epoch = 100;
+n = 512;
+max_epoch = 50000;
 
-gameYoung = [ 0.28, 0;
-              0.28, 0];
+
 
 gameOld = [ -1, -1;
             0, 0];
         
-adjmx = adjList2Mx(makeGraph(n,'random-regular',4));
-w = .25;
+
+w = 1/4;
 
 p_deffect = .5;
 strats = (rand(n,1)<p_deffect)+1;
-mat_ages = ones(n,1);
+mat_ages = randi(10,n,1);
 genotypes = [strats,mat_ages];
 
-pmod = .01;
-pmut = [.01, .01];
+pmod = .05;
+pmut = [.0001, .01];
 
 reproduce = @(agent) indMutReproduce(agent, pmut);
 
 updateRule = @deathBirth;
 
+for i=0:10
 
+gameYoung = [ i/10, 0;
+              i/10, 0];
+adjmx = adjList2Mx(makeGraph(n,'random_regular',4));
 tic
 data = pig(adjmx,genotypes,zeros(n,1),zeros(n,1),gameYoung,gameOld, w, updateRule, max_epoch,pmod,reproduce);
 toc
+
+datadir = '/home/2007/kbrown53/pigdata/';
+dataname = 'random_regular';
+data_files = dir([datadir,dataname,sprintf('%02d',i),'*']);
+save_num = size(data_files,1)+1;
+save([datadir,dataname,sprintf('%04d',save_num)]);
+end
 %%
 % figure(1)
 % plot(squeeze(sum(data(:,1,:),1))','b.');
