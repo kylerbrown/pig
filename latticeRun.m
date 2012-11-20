805c635f4002c241a60b85f7a98ae3bb2c223d1c
Kyler Brown <kbrown53@lab7-5.CS.McGill.CA>
2012-11-20 10:04:32 -0500

    fixed some bugs, parallel computing should work now

--- latticeRun.m
+++ latticeRun.m
@@ -3,7 +3,7 @@
 %   Detailed explanation goes here
 
 n = 512;
-max_epoch = 50000;
+max_epoch = 50;
 
 
 
@@ -29,16 +29,16 @@ for i=0:10
 
 gameYoung = [ i/10, 0;
               i/10, 0];
-adjmx = adjList2Mx(makeGraph(n,'random_regular',4));
+adjmx = adjList2Mx(makeGraph(n,'random-regular',4));
 tic
 data = pig(adjmx,genotypes,zeros(n,1),zeros(n,1),gameYoung,gameOld, w, updateRule, max_epoch,pmod,reproduce);
 toc
 
 datadir = '/home/2007/kbrown53/pigdata/';
-dataname = 'random_regular';
-data_files = dir([datadir,dataname,sprintf('%02d',i),'*']);
+dataname = 'random-regular';
+data_files = dir([datadir,dataname,sprintf('%03d',i),'*']);
 save_num = size(data_files,1)+1;
-save([datadir,dataname,sprintf('%04d',save_num)]);
+save([datadir,dataname,sprintf('%02d',i),'_',sprintf('%04d',save_num)]);
 end
 %%
 % figure(1)
